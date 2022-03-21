88ae31b56b04a103ed5c82753a5edbd2a1d44e79
Trigun Joshi <trigunjoshi@gmail.com>
2022-03-20 19:01:49 -0700

    new todocomponent

---
+++ react-ecosystems/src/todos/NewTodoForm.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import './NewTodoForm.css';
+
+
+const NewTodoForm = () => {
+  const [inputValue, setInputValue] = useState("")
+  return (
+    <div className='new-todo-form'>
+      <input
+        className='new-todo-input'
+        type="text" value={inputValue}
+        placeholder="Type your new todo here"
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button className='new-todo-button' type='button'></button>
+    </div>
+  )
+}
+
+export default NewTodoForm
