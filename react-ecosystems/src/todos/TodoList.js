27305356a58bfcfa4814c913171d9fb2ef07f592
Trigun Joshi <trigunjoshi@gmail.com>
2022-03-20 21:31:19 -0700

    connecting comps to the store

--- react-ecosystems/src/todos/TodoList.js
+++ react-ecosystems/src/todos/TodoList.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import NewTodoForm from './NewTodoForm';
 import TodoListItem from "./TodoListItem";
+import { connect } from 'react-redux';
 import './TodoList.css';
+import { removeTodo } from './actions';
 
-const TodoList = ({ todos = [{ text: 'hello' }] }) => {
+const TodoList = ({ todos = [], onRemovePressed }) => {
   return (
     <div className='list-wrapper'>
       <NewTodoForm />
-      {todos.map(todo => <TodoListItem todo={todo} />)}
+      {
+        todos.map(
+          todo => <TodoListItem todo={todo} onRemovePressed={onRemovePressed} />
+        )}
     </div>
   )
 }
 
-export default TodoList
+const mapStateToProps = (state) => ({
+  todos: state.todos,
+
+})
+const mapDispatchToProps = dispatch => ({
+  onRemovePressed: text => dispatch(removeTodo(text))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList)
