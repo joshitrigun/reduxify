f19c6c4599070a00ad0276e79b49c8e84694be37
Trigun Joshi <trigunjoshi@gmail.com>
2022-03-20 19:02:22 -0700

    css todocomponent

---
+++ react-ecosystems/src/todos/TodoList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import TodoListItem from "./TodoListItem";
+import './TodoList.css';
+
+const TodoList = ({ todos }) => {
+  return (
+    <div className='list-wrapper'>
+      {todos.map(todo => <TodoListItem todo={todo} />)}
+    </div>
+  )
+}
+
+export default TodoList
